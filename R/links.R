8966dfc72d4ffc184829e6f0bd14b819eed60e0a
martinbryan <bryandmartin1@gmail.com>
2018-01-01 12:33:56 -0800

    Add link functions, update description

---
+++ R/links.R
@@ -0,0 +1,75 @@
+#' Logit transformation
+#'
+#' @param x data
+#'
+#' @return logit of x
+#'
+#' @examples
+#' x <- .5
+#' logit(x)
+#'
+#' @export
+logit <- function(x) {
+  return(stats::qlogis(x))
+}
+
+
+#' Fisher's z transformation
+#'
+#' @param x data
+#'
+#' @return Fisher's z transformation of x
+#'
+#' @examples
+#' x <- .5
+#' fishZ(x)
+#'
+#' @export
+fishZ <- function(x) {
+  return(0.5 * log((1 + x)/(1 - x)))
+}
+
+#' Inverse logit transformation
+#'
+#' @param x data
+#'
+#' @return Inverse logit transformation of x
+#'
+#' @examples
+#' x <- .5
+#' invlogit(x)
+#'
+#' @export
+invlogit <- function(x) {
+  return(stats::plogis(x))
+}
+
+#' Inverse Fisher's z transformation
+#'
+#' @param x data
+#'
+#' @return Inverse Fisher's z transformation of x
+#'
+#' @examples
+#' x <- .5
+#' invfishZ(x)
+#'
+#' @export
+invfishZ <- function(x) {
+  return((exp(2 * x) - 1)/(1 + exp(2 * x)))
+}
+
+#' Hyperbolic cotangent transformation
+#'
+#' @param x data
+#'
+#' @return Hyperbolic cotangent transformation of x
+#'
+#' @examples
+#' x <- .5
+#' coth(x)
+#'
+#' @export
+coth <- function(x) {
+  return(c(1/tanh(x)))
+}

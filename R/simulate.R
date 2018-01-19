bd2d7839ffe8e97c61b9bc86a6c44c3754c85861
Bryan D Martin <bryandmartin1@gmail.com>
2018-01-18 17:01:17 -0800

    Add simulate.R manually for some reason

---
+++ R/simulate.R
@@ -0,0 +1,15 @@
+#' Simulate from BBD model fit
+#'
+#' @param object model output from \code{\link{bbdml}}
+#' @param nsim number of simulations
+#' @param seed object specifying random seed if desired
+#' @param ... optional additional arguments
+#'
+#' @return nsim simulations from mod
+#'
+#' @importFrom stats simulate
+#' @export
+simulate.bbdml <- function(object, nsim, seed, ...) {
+  if (!is.null(seed)) {set.seed(seed)}
+  return(VGAM::rbetabinom(n = nsim, size = object$M, prob = object$mu.resp, rho = object$phi.resp))
+}

4c7cd0227e0b3c1b7bc1f33d122f7c9f4f59d5e9
martinbryan <bryandmartin1@gmail.com>
2018-02-19 14:43:31 -0800

    Update dbetabin to match gradients

--- R/dbetabin.R
+++ R/dbetabin.R
@@ -34,28 +34,32 @@ dbetabin_i <- function(a1, a2, W, M, logpar = TRUE) {
 dbetabin <- function(theta, W, M, X, X_star, np, npstar, link, phi.link, logpar = TRUE) {
 
   # extract matrix of betas (np x 1), first np entries
-  b <- utils::head(theta, np)
-
-
+  b      <- utils::head(theta, np)
   # extract matrix of beta stars (npstar x 1), last npstar entries
   b_star <- utils::tail(theta, npstar)
 
-  k      <- c(exp(X %*% b) + 1)
-  k_star <- c(exp(2 * (X_star %*% b_star)) - 1)
-  a2     <- 2 / (k * k_star)
+  mu.withlink <- X %*% b
+  phi.withlink <- X_star %*% b_star
+  mu <- switch(link, "logit" = invlogit(mu.withlink))
+  phi <- switch(phi.link, "fishZ" = invfishZ(phi.withlink))
+
+  gam <- phi/(1 - phi)
+
+  a1 <- mu/gam
+  a2 <- (1 - mu)/gam
   #if (sum(a2) == Inf || any(a2 < 0)) {
   #if (sum(a2) == Inf) {
-  if (any(k_star == 0)) {
-    # no overdispersion
-    val <- sum(stats::dbinom(W, M, (k - 1)/k, log = TRUE))
-    return(-val)
-  }
+  # if (any(k_star == 0)) {
+  #   # no overdispersion
+  #   val <- sum(stats::dbinom(W, M, (k - 1)/k, log = TRUE))
+  #   return(-val)
+  # }
   if (any(a2 < 0)) {
     # want bad value just for optim, large positive for minimization
     return(1e9)
   }
 
-  a1     <- a2 * (k - 1)
+  #a1     <- a2 * (k - 1)
 
   # val <- sum(mapply(dbetabin_i,
   #                   a1 = a1, a2 = a2, W = W, M = M,

b8b24ab485126bc833108872e62907e0983cf11c
martinbryan <bryandmartin1@gmail.com>
2018-02-19 22:53:30 -0800

    Add hessian function!

---
+++ R/hessian.R
@@ -0,0 +1,86 @@
+#' Compute Hessian
+#'
+#' @param mod model fit from bbdml
+#' @param numerical Boolean numerical Hessian. Not as stable. Defaults to FALSE
+#'
+#' @return Analytic Hessian
+#'
+#' @examples
+#' \dontrun{
+#' TODO
+#' }
+#'
+#' @export
+hessian <- function(mod, numerical = FALSE) {
+  mu <- out$mu.resp
+  phi <- out$phi.resp
+  gam <- phi/(1 - phi)
+  N <- out$dat$Wi
+  M <- out$dat$Ni
+  X <- out$X.mu
+  W <- out$X.phi
+  
+  
+  npx <- ncol(X)
+  npw <- ncol(W)
+  H <- matrix(0, nrow = npx + npw, ncol = npx + npw)
+  
+  if (numerical) {
+    return(hessian(dbetabin,x=out$param,W=N,M=M,X=X,X_star=W,np=npx,npstar=npw,link=out$link,phi.link=out$phi.link))
+  }
+  
+  # This part is fully generalized
+  for (i in 1:length(mu)) {
+    m <- mu[i]
+    g <- gam[i]
+    y <- N[i]
+    n <- M[i]
+    x <- X[i,]
+    w <- W[i,]
+    tg1 <- trigamma(n - (m + y*g - 1)/g)
+    tg2 <- trigamma((1 - m)/g)
+    tg3 <- trigamma(m/g)
+    tg4 <- trigamma(m/g + y)
+    
+    tg5 <- trigamma(1/g)
+    tg6 <- trigamma(n + 1/g)
+    
+    dg1 <- digamma(1/g)
+    dg2 <- digamma(n + 1/g)
+    dg3 <- digamma(n - (m + y*g - 1)/g)
+    dg4 <- digamma((1 - m)/g)
+    dg5 <- digamma(m/g)
+    dg6 <- digamma(m/g + y)
+    
+    # Generalizable single derivatives dL
+    dldmu <- (-dg3 + dg4 - dg5 + dg6)/g
+    dldgam <- (-dg1 + dg2 + (m - 1)*(dg3 - dg4) + m*(dg5 - dg6))/g^2
+    # Generalizable double derivatives
+    dldmu2 <- (tg1 - tg2 - tg3 + tg4)/g^2
+    dldgam2 <- (2*g*dg1 + tg5 - 2*g*dg2 - tg6 + (m - 1)^2*tg1 - 
+                  2*g*(m - 1)*dg3 - m^2*tg3 + m^2*tg4 + (m - 1)^2*(-tg2) + 
+                  2*g*(m - 1)*dg4 - 2*g*m*dg5 + 2*g*m*dg6)/g^4
+    dldmdg <- (g*(dg3 - dg4 + dg5 - dg6) + (m - 1)*(tg2 - tg1) + m*(tg3 - tg4))/g^3
+    
+    # Not generalizeable single dm and dg
+    dpdb <- x * m * (1 - m)
+    dgdb <- w * (g + 0.5)
+    
+    dpdb <- c(dpdb, rep(0, npw))
+    dgdb <- c(rep(0, npx), dgdb)
+    # Not generalizable double 
+    dpdb2 <- tcrossprod(x) * m * (1 - m) * (1 - 2 * m)
+    dgdb2 <- tcrossprod(w) * (g + 0.5)
+    
+    dpdb2 <- as.matrix(Matrix::bdiag(dpdb2, matrix(0, nrow = npw, ncol = npw)))
+    dgdb2 <- as.matrix(Matrix::bdiag(matrix(0, nrow = npx, ncol = npx), dgdb2))
+    
+    term1 <- (-dldmu2) * tcrossprod(dpdb)
+    term2 <- (-dldmdg) * (tcrossprod(dpdb, dgdb) + tcrossprod(dgdb, dpdb))
+    term3 <- (-dldgam2) * tcrossprod(dgdb)
+    term4 <- dldmu*dpdb2
+    term5 <- dldgam*dgdb2
+    H <- H + term1 + term2 + term3 - term4 - term5
+  }
+  return(H)
+}

48385fe4afc91dedde94ef13988742fa57a027f5
martinbryan <bryandmartin1@gmail.com>
2018-04-02 12:20:32 -0700

    Fix CMD CHECK warnings

--- R/plot_bbdml.R
+++ R/plot_bbdml.R
@@ -1,7 +1,7 @@
 #' Plotting function (in beta)
 #'
-#' @param mod Output from \code{\link{bbdml}}
-#' @param RA Boolean indicator for whether to plot on relative abundance scale. Defaults to FALSE.
+#' @param x Output from \code{\link{bbdml}}
+#' @param ... Optional boolean indicator called RA for whether to plot on relative abundance scale. Defaults to FALSE.
 #'
 #' @return Model plot
 #'
@@ -10,7 +10,11 @@
 #' TODO
 #' }
 #' @export
-plot.bbdml <- function(mod, RA = FALSE) {
+plot.bbdml <- function(x, ...) {
+  mod <- x
+  if (!exists("RA")) {
+    RA <- FALSE
+  }
   mu_est <- mod$mu.resp
   phi_est <- mod$phi.resp
   M <- mod$M
@@ -22,14 +26,14 @@ plot.bbdml <- function(mod, RA = FALSE) {
                         index = 1:length(M)
     )
 
-    ggplot(df_RA, aes(x = index, y = RA)) +
-      geom_point() +
-      geom_point(aes(x = index, y = E_RA), pch = 2) +
-      geom_errorbar(aes(ymin = E_RA - 2*SE_RA, ymax = E_RA + 2*SE_RA), width = .2,
-                    position = position_dodge(.05)) +
-      labs(x = "Sample", y = "Relative Abundance", title = "BBD Model Fit") +
-      theme_bw() +
-      theme(plot.title = element_text(hjust = 0.5))
+    ggplot2::ggplot(df_RA, ggplot2::aes(x = index, y = RA)) +
+      ggplot2::geom_point() +
+      ggplot2::geom_point(ggplot2::aes(x = index, y = E_RA), pch = 2) +
+      ggplot2::geom_errorbar(ggplot2::aes(ymin = E_RA - 2*SE_RA, ymax = E_RA + 2*SE_RA), width = .2,
+                    position = ggplot2::position_dodge(.05)) +
+      ggplot2::labs(x = "Sample", y = "Relative Abundance", title = "BBD Model Fit") +
+      ggplot2::theme_bw() +
+      ggplot2::theme(plot.title = ggplot2::element_text(hjust = 0.5))
   } else {
     df_RA <- data.frame(RA = W,
                         E_RA = mu_est*M,
@@ -37,14 +41,14 @@ plot.bbdml <- function(mod, RA = FALSE) {
                         index = 1:length(M)
     )
 
-    ggplot(df_RA, aes(x = index, y = RA)) +
-      geom_point() +
-      geom_point(aes(x = index, y = E_RA), pch = 2) +
-      geom_errorbar(aes(ymin = E_RA - 2*SE_RA, ymax = E_RA + 2*SE_RA), width = .2,
-                    position = position_dodge(.05)) +
-      labs(x = "Sample", y = "Absolute Abundance", title = "BBD Model Fit") +
-      theme_bw() +
-      theme(plot.title = element_text(hjust = 0.5))
+    ggplot2::ggplot(df_RA, ggplot2::aes(x = index, y = RA)) +
+      ggplot2::geom_point() +
+      ggplot2::geom_point(ggplot2::aes(x = index, y = E_RA), pch = 2) +
+      ggplot2::geom_errorbar(ggplot2::aes(ymin = E_RA - 2*SE_RA, ymax = E_RA + 2*SE_RA), width = .2,
+                    position = ggplot2::position_dodge(.05)) +
+      ggplot2::labs(x = "Sample", y = "Absolute Abundance", title = "BBD Model Fit") +
+      ggplot2::theme_bw() +
+      ggplot2::theme(plot.title = ggplot2::element_text(hjust = 0.5))
   }
 
 

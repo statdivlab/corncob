cd9ba30c50856ffb772dd0e72f3f84ad549dc621
martinbryan <bryandmartin1@gmail.com>
2018-04-17 14:03:11 -0700

    Decrease tolerance for numerical rounding

--- R/objfun.R
+++ R/objfun.R
@@ -29,7 +29,7 @@ objfun <- function(theta, W, M, X, X_star, np, npstar, link, phi.link) {
 
   if (any(mu < 0) || any(mu > 1)) {
     return(list(value = Inf))
-  } else if (any(phi <= .Machine$double.eps) || any(phi == 1)) {
+  } else if (any(phi <= 1000*.Machine$double.eps) || any(phi >= 1 - 1000*.Machine$double.eps)) {
     return(list(value = Inf))
   } else {
     gam <- phi/(1 - phi)

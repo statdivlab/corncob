0eed09e8d0cff0b95479a16a4821daf47ade36b0
Bryan Martin <bmartin6@uw.edu>
2018-07-25 12:52:26 -0700

    Add phyloseq soil data

---
+++ R/soil_phylo.R
@@ -0,0 +1,7 @@
+#' Soil data.
+#'
+#' A phyloseq object with an OTU table and sample data from a soil microbiome study.
+#'
+#' @format A phyloseq-class experiment-level object with an OTU table and sample data.
+#' @references Whitman, T., Pepe-Ranney, C., Enders, A., Koechli, C., Campbell, A.,  Buckley, D. H., Lehmann, J. (2016). \emph{Dynamics of microbial community composi-tion and soil organic carbon mineralization in soil following addition of pyrogenic andfresh organic matter}. The ISME journal, 10(12):2918.
+"soil_phylo"

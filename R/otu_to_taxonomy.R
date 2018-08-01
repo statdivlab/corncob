fba40393df7847972cf43d716927db39ee152c45
Bryan Martin <bmartin6@uw.edu>
2018-07-31 20:33:29 -0400

    Update vignette, add otu_to_taxonomy. Clean soil_phylo.

---
+++ R/otu_to_taxonomy.R
@@ -0,0 +1,18 @@
+#' Transform OTUs to their taxonomic label
+#'
+#' @param OTU String vector. Names of OTU labels in \code{data}
+#' @param data \code{phyloseq} object with a taxonomy table
+#'
+#' @importFrom magrittr %>%
+#'
+#' @return String vector. Names of taxonomic labels matching labels of \code{OTU}.
+#'
+#' @examples
+#' \dontrun{
+#' TODO
+#' }
+#'
+#' @export
+otu_to_taxonomy <- function(OTU, data) {
+  return(apply(tax_table(data), 1, function(x) {paste(na.omit(x), collapse = '_')}))
+}

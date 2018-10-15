964ad1d8e36a5e9b284678c4a8a07d7c0e8389b3
Bryan Martin <bmartin6@uw.edu>
2018-10-15 13:05:00 -0700

    Add inits to bootstrap for speed improvement

--- R/doBoot.R
+++ R/doBoot.R
@@ -19,11 +19,11 @@ doBoot <- function(mod, mod_null, test) {
   newout_null <- try(bbdml(formula = mod_null$formula,
                        phi.formula = mod_null$phi.formula,
                        link = mod_null$link, phi.link = mod_null$phi.link,
-                       data = newdf), silent = TRUE)
+                       data = newdf, inits = mod_null$inits), silent = TRUE)
   newout_alt <- try(bbdml(formula = mod$formula,
                       phi.formula = mod$phi.formula,
                       link = mod$link, phi.link = mod$phi.link,
-                      data = newdf), silent = TRUE)
+                      data = newdf, inits = mod$inits), silent = TRUE)
 
   if ("try-error" %in% c(class(newout_null), class(newout_alt))) {
     return(NA)

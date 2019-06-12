3ac872893d184f27ca5cce21de0a4c4049a6a007
Bryan Martin <bmartin6@uw.edu>
2019-06-12 12:13:15 -0700

    Fix citations for ibd data

--- R/ibd_phylo.R
+++ R/ibd_phylo.R
@@ -3,7 +3,7 @@
 #' A phyloseq object with an OTU table and sample data from an IBD microbiome study.
 #'
 #' @format A phyloseq-class experiment-level object with an OTU table and sample data.
-#' @references Duvallet, C., Gibbons, S. M., Gurry, T., Irizarry, R. A., & Alm, E. J. (2017). \emph{Meta-analysis of gut microbiome studies identifies disease-specific and shared responses}. Nature communications, 8(1), 1784. <doi.org/10.1038/s41467-017-01973-8>.
+#' @references Papa, E., Docktor, M., Smillie, C., Weber, S., Preheim, S. P., Gevers, D., Giannoukos, G., Ciulla, D., Tabbaa, D., Ingram, J., Schauer, D. B., Ward, D. V., Korzenik, J. R., Xavier, R. J., Bousvaros, A., Alm, E. J. & Schauer, D. B. (2012). \emph{Non-invasive mapping of the gastrointestinal microbiota identifies children with inflammatory bowel disease}. PloS One, 7(6), e39242. <doi.org/10.1371/journal.pone.0039242>.
 #'
 #' Duvallet, C., Gibbons, S., Gurry, T., Irizarry, R., & Alm, E. (2017). \emph{MicrobiomeHD: the human gut microbiome in health and disease [Data set]}. Zenodo. <doi.org/10.5281/zenodo.1146764>.
 "ibd_phylo"

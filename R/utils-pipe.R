da810dd4c725f77f2bb83f0a90433abfb9015ae2
Bryan Martin <bryandmartin1@gmail.com>
2021-03-05 14:17:30 -0800

    Capitalize DOI and add pipe for CRAN

---
+++ R/utils-pipe.R
@@ -0,0 +1,12 @@
+#' Pipe operator
+#'
+#' See \code{magrittr::\link[magrittr:pipe]{\%>\%}} for details.
+#'
+#' @name %>%
+#' @rdname pipe
+#' @keywords internal
+#' @return No return value. Created to import the pipe operator.
+#' @export
+#' @importFrom magrittr %>%
+#' @usage lhs \%>\% rhs
+NULL

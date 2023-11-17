6fcb24ca499e2718efd690461d8762c87fc65445
Sarah Teichman <teichs@uw.edu>
2023-11-17 13:09:24 -0800

    update example in `contrastsTest()` to only try the first 5 taxa in order to save time when running examples

--- R/wald.R
+++ R/wald.R
@@ -116,7 +116,7 @@ waldchisq_test <- function(mod, restrictions = NULL, restrictions.phi = NULL,
   } else {
 
     if (!is.null(contrasts_DA)) {
-      requireNamespace("limma")
+      #requireNamespace("limma")
       contr_vec <- suppressWarnings(limma::makeContrasts(contrasts = contrasts_DA, levels = colnames(mod$X.mu)))
       contr_vec <- c(contr_vec, rep(0, mod$np.phi))
     } else if (!is.null(contrasts_DV)) {

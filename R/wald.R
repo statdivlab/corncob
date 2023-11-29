cb9920bd36d0252dee7453999e9254103fc21cd3
Sarah Teichman <teichs@uw.edu>
2023-11-29 14:39:22 -0800

    use the smaller dataset for the example to avoid example code taking >5 seconds to run

--- R/wald.R
+++ R/wald.R
@@ -6,13 +6,10 @@
 #'
 #'
 #' @examples
-#' data(soil_phylo)
-#' soil <- soil_phylo %>%
-#' phyloseq::subset_samples(DayAmdmt %in% c(11,21)) %>%
-#' phyloseq::tax_glom("Phylum")
+#' data(soil_phylum_small)
 #' mod1 <- bbdml(formula = OTU.1 ~ DayAmdmt,
 #' phi.formula = ~ DayAmdmt,
-#' data = soil)
+#' data = soil_phylum_small)
 #' waldt(mod1)
 #'
 #' @export

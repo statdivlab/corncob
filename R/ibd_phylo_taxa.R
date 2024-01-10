89309a07ff904e6a4050579e56bb607c61c3500a
Sarah Teichman <teichs@uw.edu>
2024-01-09 16:27:37 -0800

    saving ibd data from phyloseq object as three separate data frames

---
+++ R/ibd_phylo_taxa.R
@@ -0,0 +1,9 @@
+#' IBD data, taxonomy data frame.
+#'
+#' Taxonomy data from a phyloseq object from an IBD microbiome study.
+#'
+#' @format A data frame of taxonomy data.
+#' @references Papa, E., Docktor, M., Smillie, C., Weber, S., Preheim, S. P., Gevers, D., Giannoukos, G., Ciulla, D., Tabbaa, D., Ingram, J., Schauer, D. B., Ward, D. V., Korzenik, J. R., Xavier, R. J., Bousvaros, A., Alm, E. J. & Schauer, D. B. (2012). \emph{Non-invasive mapping of the gastrointestinal microbiota identifies children with inflammatory bowel disease}. PloS One, 7(6), e39242. <doi.org/10.1371/journal.pone.0039242>.
+#'
+#' Duvallet, C., Gibbons, S., Gurry, T., Irizarry, R., & Alm, E. (2017). \emph{MicrobiomeHD: the human gut microbiome in health and disease [Data set]}. Zenodo. <doi.org/10.5281/zenodo.1146764>.
+"ibd_phylo_taxa"
